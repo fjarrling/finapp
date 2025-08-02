d3acc8a6910957a3993c6699d24fc29a407bbdab
Eduard Kosian <fjarrling@gmail.com>
2025-08-02 16:19:37 +0300

    feat: на странице аккаунты сделал вывод текста если аккаунтов еще нет

--- src/pages/Accounts/Accounts.tsx
+++ src/pages/Accounts/Accounts.tsx
@@ -3,7 +3,8 @@ import Header from "@/components/Header";
 import {useAppSelector} from "@/store/store.ts";
 import {selectAllAccounts} from "@/store/accountsSlice.ts";
 import AccountCard from "@/components/AccountCard";
-import AddAccountDialog from "@/components/AddAccountDialog";
+import FormDialog from "@/components/FormDialog";
+import AddAccountForm from "@/components/AddAccountForm";
 
 const Accounts = () => {
   const accounts = useAppSelector(selectAllAccounts)
@@ -16,15 +17,21 @@ const Accounts = () => {
             <h1>
               My Accounts
             </h1>
-            <AddAccountDialog/>
+            <FormDialog Form={AddAccountForm} title="Add Account"/>
           </div>
-          <div className='w-full grid grid-cols-2 gap-4 mb-4 lg:grid-cols-3'>
-            {
-              accounts.map((account) => (
-                <AccountCard key={account.id} account={account}/>
-              ))
-            }
-          </div>
+          {!accounts.length &&
+            <div className="w-full flex items-center justify-center py-16 text-2xl">
+              You have not added accounts yet
+            </div>}
+          {accounts.length > 0 &&
+            <div className='w-full grid grid-cols-2 gap-4 mb-4 lg:grid-cols-3'>
+              {
+                accounts.map((account) => (
+                  <AccountCard key={account.id} account={account}/>
+                ))
+              }
+            </div>
+          }
         </div>
       </div>
     </>
