16f5f20d481e631545a1ec13c61602ae605ffde4
Eduard Kosian <fjarrling@gmail.com>
2025-08-26 17:11:13 +0300

    fix: убрал отображение последних транзакций если их нет

--- src/pages/Dashboard/LastTransactions/LastTransactions.tsx
+++ src/pages/Dashboard/LastTransactions/LastTransactions.tsx
@@ -11,34 +11,39 @@ const LastTransactions = () => {
   const lastTransactions = useAppSelector((selectLastNTransactions(5)))
 
   return (
-    <div className="rounded-xl border p-4 md:p-6">
-      <h3 className="text-xl mb-4 md:mb-8">Last transactions</h3>
-      <div className="space-y-3">
-        {
-          lastTransactions.map((transaction) => {
-            const category = categoriesMap[transaction.categoryId]
-            const account = accountsMap[transaction.accountId]
-            return (
-              <div key={transaction.id}
-                   className="flex items-center justify-between py-2 border-b border-border last:border-b-0">
-                <div className="flex items-center">
-                  <div className={`w-3 h-3 rounded-full mr-3 ${category.color}`}></div>
-                  <div>
-                    <p className="font-medium">{category.name}</p>
-                    <p className="text-sm text-muted-foreground">
-                      {format(transaction.date, "PPP")}
-                    </p>
+    <>
+      {
+        lastTransactions.length > 0 &&
+        <div className="rounded-xl border p-4 md:p-6">
+          <h3 className="text-xl mb-4 md:mb-8">Last transactions</h3>
+          <div className="space-y-3">
+            {lastTransactions.map((transaction) => {
+              const category = categoriesMap[transaction.categoryId]
+              const account = accountsMap[transaction.accountId]
+              return (
+                <div key={transaction.id}
+                     className="flex items-center justify-between py-2 border-b border-border last:border-b-0">
+                  <div className="flex items-center">
+                    <div className={`w-3 h-3 rounded-full mr-3 ${category.color}`}></div>
+                    <div>
+                      <p className="font-medium">{category.name}</p>
+                      <p className="text-sm text-muted-foreground">
+                        {format(transaction.date, "PPP")}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                <span className={`font-semibold ${transaction.type === 'income' ? 'text-green-600' : 'text-red-600'}`}>
+                  <span
+                    className={`font-semibold ${transaction.type === 'income' ? 'text-green-600' : 'text-red-600'}`}>
                   {transaction.amount} {getCurrencySymbol(account.currency)}
                 </span>
-              </div>
-            )
-          })
-        }
-      </div>
-    </div>
+                </div>
+              )
+            })}
+          </div>
+        </div>
+      }
+    </>
+
   );
 };
 
